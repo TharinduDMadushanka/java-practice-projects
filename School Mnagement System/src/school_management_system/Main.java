bcd38cf0e4a97de69cfaa8acd75378a13f8f75a4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-04-26 21:42:34 +0530

    update all clasess

---
+++ School Mnagement System/src/school_management_system/Main.java
@@ -0,0 +1,29 @@
+package school_management_system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher Lizzy = new Teacher(1, "Lizzy", 500);
+        Teacher Melissa = new Teacher(2, "Melissa", 700);
+        Teacher Jhon = new Teacher(3, "Jhon", 600);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(Lizzy);
+        teacherList.add(Melissa);
+        teacherList.add(Jhon);
+
+        Student Vince = new Student(1, "Vince", 4);
+        Student Tharindu = new Student(2, "Tharindu", 10);
+        Student rooby = new Student(3, "Rooby", 5);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(Vince);
+        studentList.add(Tharindu);
+        studentList.add(rooby);
+
+        School ghs = new School(teacherList, studentList);
+        System.out.println("GHS has earned: $ " + ghs.getTotalMoneyEarned());
+    }
+}
--- School Mnagement System/src/school_management_system/School.java
+++ School Mnagement System/src/school_management_system/School.java
@@ -1,12 +1,59 @@
 package school_management_system;
 
+import java.util.List;
+
 /**
  * Many teachers, many students
  * Implement teachers and students using ArrayLists
  */
 public class School {
-    private Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers; // List of teachers (unknown numbers)
+    private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
+
+    /**
+     * new school object is created
+     *
+     * @param teachers--> list of teachers in the school
+     * @param students    --> list of students in the school
+     */
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {// returns the list of teachers in the school
+        return teachers;
+    }
+
+    public void addTeachers(Teacher teacher) { //add teachers to the school
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {// returns the list of students in the school
+        return students;
+    }
+
+    public void addStudents(Student student) {// add student to the school
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() { // return the total money earned by school
+        return totalMoneyEarned;
+    }
+
+    public void updateTotalMoneyEarned(int MoneyEarned) { // add the total money earned by school
+        this.totalMoneyEarned += MoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {// return the total money spent by school
+        return totalMoneySpent;
+    }
+
+    public void updateTotalMoneySpent(int MoneySpent) {// update  money that is spent by school (salary of scl teachers)
+        totalMoneyEarned -= MoneySpent;
+    }
 }
