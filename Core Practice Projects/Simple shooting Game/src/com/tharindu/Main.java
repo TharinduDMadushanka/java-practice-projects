c45eccf7053252b5f5033517cd09ffea2bd9ed3e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-04-29 09:38:34 +0530

    modified sms

---
+++ Core Practice Projects/Simple shooting Game/src/com/tharindu/Main.java
@@ -0,0 +1,10 @@
+package com.tharindu;
+
+public class Main {
+    public static void main(String[] args) {
+        Player1 player = new Player1("Thari", "sword", 500);
+//        System.out.println(player.getName());
+        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+    }
+}
---
+++ Core Practice Projects/Simple shooting Game/src/com/tharindu/Player1.java
@@ -0,0 +1,63 @@
+package com.tharindu;
+
+public class Player1 {
+    private String name;
+    private String weapon;
+    private int health;
+
+    public Player1(String name, String weapon, int health) {
+        this.name = name;
+        this.weapon = weapon;
+        if (health<0 || health>100){
+            this.health = 100;
+        }else {
+            this.health=health;
+        }
+    }
+
+    public void damageByGun1() {
+        this.health -= 30;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+        System.out.println("Got hit by gun 1. Health is reduce by 30%. New health is " + this.health);
+        if (this.health == 0) {
+            System.out.println(getName()+" is dead.");
+        }
+    }
+
+    public void damageByGun2() {
+        this.health -= 50;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+        System.out.println("Got hit by gun 2. Health is reduce by 50%. New health is " + this.health);
+        if (this.health == 0) {
+            System.out.println(getName()+" is dead.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
