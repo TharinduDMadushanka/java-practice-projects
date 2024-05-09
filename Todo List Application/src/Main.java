471da11b0ebc8201c499498a4779b628cf910477
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-09 22:04:26 +0530

    first commit

--- Todo List Application/src/Main.java
+++ Todo List Application/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        menuLine();
+    }
+
+    public static void menuLine() {
+        for (int i = 0; i < 40; i++) {
+            System.out.print("-");
+        }
+    }
+
+    public static void menu() {
+
     }
 }
