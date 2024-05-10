11d5fb8f04c4946ee2e9b71a258574b5ed2262c0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-10 08:24:05 +0530

    first commit

---
+++ Todo List Application/src/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    private String description;
+    private String dueDate;
+
+    public Task(String description,String dueDate){
+        this.description=description;
+        this.dueDate=dueDate;
+    }
+}
